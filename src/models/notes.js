2635c27bb5bb4b4b10846070bac73fb5145ba1c7
ARTEM POBEHAILO <partemishe@gmail.com>
2025-09-22 17:05:26 +0300

    doing hw

---
+++ src/models/notes.js
@@ -0,0 +1,39 @@
+// src/models/notes.js
+
+import { model, Schema } from 'mongoose';
+
+const notesSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    tag: {
+      type: String,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Note = model('Note', notesSchema);
